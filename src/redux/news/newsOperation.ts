6852ab26eebc43fdd24642fd5f91d407975cc449
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2024-03-16 11:25:02 +0200

    pagination styles fixed

--- src/redux/news/newsOperation.ts
+++ src/redux/news/newsOperation.ts
@@ -6,7 +6,6 @@ axios.defaults.baseURL = 'https://petlove.b.goit.study/api';
 export const getAllNews = createAsyncThunk(
   'news/getAllNews',
   async (data: number, thunkAPI) => {
-    console.log(data);
     try {
       const response = await axios.get(`/news?page=${data}`);
       return response.data;
